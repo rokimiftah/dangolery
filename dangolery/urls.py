81a04b1b4ae17742ccea4c32a2744ad7f1c83f39
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-21 17:20:50 +0700

    feat: add page add picture

--- dangolery/urls.py
+++ dangolery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from dangolery.views import index
+from dangolery.views import add, index
 
 urlpatterns = [
     path("", index, name="index"),
+    path("add/", add, name="add"),
 ]
--- dangolery/views.py
+++ dangolery/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "dangolery/index.html")
+
+
+def add(request):
+    return render(request, "dangolery/add.html")
