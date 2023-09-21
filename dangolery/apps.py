07ab3ca4007f1bf519b5e9073afc66409e4c4f8f
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-21 16:33:41 +0700

    feat: init dangolery app

--- dangolery/apps.py
+++ dangolery/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DangoleryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dangolery'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dangolery"
